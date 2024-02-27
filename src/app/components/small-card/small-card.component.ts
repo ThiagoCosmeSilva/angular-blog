ea445a0ed8447a945cfcabf9033dd9788bd9b9ab
Thiago Cosme <thiago.cosme@hotmail.com>
2024-02-27 20:52:33 -0300

    Componentes dinamicos

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css'],
 })
 export class SmallCardComponent implements OnInit {
+  photoCover: string =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7NNEJ33G38KYccnGvalx81zL8bY2iKyBvHi-1I4lZYYEdeaIf2hOOcxPAAM-4xbF8Lf0&usqp=CAU';
+  cardTitle: string = 'Nova serie anunciada';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
