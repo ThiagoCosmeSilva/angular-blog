e059d57a47743e8a65243e6496fbe06cc31a923a
Thiago Cosme <thiago.cosme@hotmail.com>
2024-02-27 22:32:20 -0300

    Projeto Final

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,9 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css'],
 })
 export class SmallCardComponent implements OnInit {
-  photoCover: string =
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7NNEJ33G38KYccnGvalx81zL8bY2iKyBvHi-1I4lZYYEdeaIf2hOOcxPAAM-4xbF8Lf0&usqp=CAU';
-  cardTitle: string = 'Nova serie anunciada';
+  @Input()
+  photoCover: string = '';
+
+  @Input()
+  cardTitle: string = '';
+
+  @Input()
+  Id: string = '0';
+
   constructor() {}
 
   ngOnInit(): void {}
---
+++ src/app/data/datafake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'NOVO FILME DO HOMEM DE FERRO EM 3D',
+    description: 'Marvel anuncia novo homem de ferro',
+    photoCover:
+      'https://res.cloudinary.com/nerd-club/image/upload/c_thumb,dpr_2.0,f_auto,h_1000,q_auto:good,w_1400/avengers-poster.jpg',
+  },
+  {
+    id: '2',
+    title: 'Nova Serie Anunciada no disney plus',
+    description: 'Bla bla bla',
+    photoCover:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7NNEJ33G38KYccnGvalx81zL8bY2iKyBvHi-1I4lZYYEdeaIf2hOOcxPAAM-4xbF8Lf0&usqp=CAU',
+  },
+];
